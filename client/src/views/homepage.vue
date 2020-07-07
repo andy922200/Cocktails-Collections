49eaf78372c8831ab2e7757cfc0a096787d010af
andysml <andy922200@gmail.com>
2020-07-07 17:45:41 +0800

    add basic random drink layout

--- client/src/views/homepage.vue
+++ client/src/views/homepage.vue
@@ -4,12 +4,38 @@
       <el-col :span="24">
         <navbar />
       </el-col>
+
       <el-col :xs="24" :span="12">
         <homepageCarousel />
       </el-col>
-      <!-- <el-col :xs="24" :span="12">
-        <carousel />
-      </el-col> -->
+      <el-col :xs="24" :span="12">
+        <span style="color:white;">SearchArea</span>
+      </el-col>
+
+      <el-col :span="24">
+        <div class="randomSelect">
+          <div class="twoColumns twoColumns--flex">
+            <div class="randomSelect__drink">
+              <img
+                :src="drinks[0].thumbImg"
+                class="randomSelect__image"
+                alt="randomDrinkImage"
+              />
+            </div>
+          </div>
+          <div class="twoColumns">
+            <div class="randomSelect__content">
+              <p class="randomSelect__title">{{ drinks[0].name }}</p>
+              <el-table :data="drinks[0].ingredients" border>
+                <el-table-column prop="name" label="Name">
+                </el-table-column>
+                <el-table-column prop="measure" label="Quantity">
+                </el-table-column>
+              </el-table>
+            </div>
+          </div>
+        </div>
+      </el-col>
     </el-row>
   </div>
 </template>
@@ -17,10 +43,20 @@
 <script>
 import navbar from "./../components/navbar";
 import homepageCarousel from "./../components/homepageCarousel";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "Homepage",
-  components: { navbar, homepageCarousel }
+  components: { navbar, homepageCarousel },
+  created() {
+    this.getARandomCocktail();
+  },
+  computed: {
+    ...mapGetters("cocktails", ["drinks", "fetchingData"])
+  },
+  methods: {
+    ...mapActions("cocktails", ["getARandomCocktail"])
+  }
 };
 </script>
 
